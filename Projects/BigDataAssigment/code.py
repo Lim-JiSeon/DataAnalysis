c8cf0878d610dac5a9df2a8f9c57f1311bcd7a0b
Lim-JiSeon <83554018+Lim-JiSeon@users.noreply.github.com>
2023-03-30 01:36:39 +0900

    Create code.py

---
+++ Projects/BigDataAssigment/code.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup as bs
+import urllib.request
+import pandas as pd
+import datetime, time
+
+num = 0
+def kyochon_store(result):
+    for sido in range(1,18):
+      for gu in range(1,46):
+        try:
+          kyochon_url = 'http://www.kyochon.com/shop/domestic.asp?txtsearch=&sido1=%s&sido2=%s' % (sido, gu)
+          html = urllib.request.urlopen(kyochon_url)
+          soupKyochon = bs(html, 'html.parser')
+          ul_tag = soupKyochon.find("div", {"class": "shopSchList"})
+
+          for store_data in ul_tag.findAll('a'):
+            store_name = store_data.find('strong').get_text()
+            store_address = store_data.find('em').get_text().strip().split('\r')[0]
+            store_sido, store_gu = store_address.split()[0], store_address.split()[1]
+            result.append([store_name] + [store_sido] + [store_gu] + [store_address])
+        except:
+          pass
+
+
+def main():
+  result = []
+  kyochon_store(result)
+  kyochon_tbl = pd.DataFrame(result, columns=('store', 'sido', 'gungu', 'store_address'))
+  kyochon_tbl.to_csv('C:\\Users\\LimJiSeon\\OneDrive\\바탕 화면\\test\\test.csv', encoding='cp949', mode='w', index=True)
+  del result[:]
+
+
+if __name__ == '__main__':
+  main()
